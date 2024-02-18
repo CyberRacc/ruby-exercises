afe93a3d4419e6acc14bdaf419939106cd969346
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-02-18 21:03:16 +0000

    Cipher almost works, but I need to account for case

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,17 @@
+def normalise_ascii_input(ascii_input)
+  # Normalise the input number to a range between 0-25
+  if ("A".ord.."Z".ord).include?(ascii_input)
+    ascii_number = ascii_input - "A".ord
+  elsif ("a".ord.."z".ord)
+    ascii_number = ascii_input - "a".ord
+  end
+
+  if ascii_number > 25
+    ascii_number %= 26
+  end
+  return ascii_number
+end
+
 def caesar_cipher(message, shift_by)
 
   # May need seperate if statements for if the letter is in upper case or lower case,
@@ -9,12 +23,12 @@ def caesar_cipher(message, shift_by)
 
   message_array.each do |substring|
 
-    if substring.match?(/[A-Za-z]/) # If the substring is alphabetical
-      message_ciphered.push((substring.ord + shift_by).chr)
-      # If the final value of substring.ord + shift_by is greater than 26
-      # Do the final value modulo % 26, this should wrap the characters back aroud the alphabet.
+    if substring.match?(/[A-Za-z]/)
+      substring_ascii_number = substring.ord
+      normalised_ascii_number = (normalise_ascii_input(substring_ascii_number) + "a".ord) + shift_by
+      message_ciphered.push(normalised_ascii_number.chr)
     else
-      message_ciphered.push(substring) # If not alphabetical, i.e. spaces, etc. Push withou modification
+      message_ciphered.push(substring)
     end
 
   end
@@ -23,7 +37,9 @@ def caesar_cipher(message, shift_by)
 
 end
 
-caesar_cipher("test", 5)
+
+
+caesar_cipher("A test", 5)
 caesar_cipher("test", 6)
 caesar_cipher("test", 3)
 
